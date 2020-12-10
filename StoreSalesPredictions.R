2a6059d7031fd685611d048411f8e15799406dc6
gtbagley <70668517+gtbagley@users.noreply.github.com>
2020-12-09 18:44:07 -0700

    Update StoreSalesPredictions.R

--- StoreSalesPredictions.R
+++ StoreSalesPredictions.R
@@ -1,17 +1,17 @@
-#load libraries
+#Load libraries
 library(tidyverse)
 library(lubridate)
 library(readr)
 library(forecast)
 
-#read in data
+#Read in data
 train <- read_csv("../input/demand-forecasting-kernels-only/train.csv")
 test <- read_csv("../input/demand-forecasting-kernels-only/test.csv")
 
-#remove unecessary variables
+#Remove unecessary variables
 train.cleaned <- train %>% select(sales, store, item)
 
-#create multi-seasonal time series model and predict sales for the next 90 days
+#Create multi-seasonal time series model and predict sales for the next 90 days. Seasonality aims to model weekly and yearly patterns.
 df_total = data.frame()
 for (y in 1:50){ #needs to be 1:50
   for (x in 1:10){ #needs to be 1:10
@@ -25,11 +25,11 @@ for (y in 1:50){ #needs to be 1:50
   }
 }
 
-#create new id variable
+#Create new id variable
 df_total$id <- seq.int(nrow(df_total))-1
 
-#reorder variables
+#Reorder variables
 submission <- df_total[c(2,1)]
 
-#write out submission file
+#Write out submission file
 write_csv(submission, "submission.csv")
