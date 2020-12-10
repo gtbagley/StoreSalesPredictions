89fd4245c7d244ab9e530cf6c57350afeaf17a4a
gtbagley <70668517+gtbagley@users.noreply.github.com>
2020-12-09 18:44:47 -0700

    Update StoreSalesPredictions.R

--- StoreSalesPredictions.R
+++ StoreSalesPredictions.R
@@ -13,8 +13,8 @@ train.cleaned <- train %>% select(sales, store, item)
 
 #Create multi-seasonal time series model and predict sales for the next 90 days. Seasonality aims to model weekly and yearly patterns.
 df_total = data.frame()
-for (y in 1:50){ #needs to be 1:50
-  for (x in 1:10){ #needs to be 1:10
+for (y in 1:50){
+  for (x in 1:10){
     traindata <- train.cleaned %>% filter(store == x) %>% filter(item == y) %>% select(sales)
     msts <- msts(data=traindata, seasonal.periods = c(7, 365.25), start = decimal_date(as.Date("2013-01-01")))
     tbats <- tbats(msts)
